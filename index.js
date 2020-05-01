05f0e772093510e07db100aa2e5090a88898f8fe
bandydos <alexandertack@hotmail.be>
2020-05-01 12:53:46 +0200

    Done

--- index.js
+++ index.js
@@ -11,19 +11,35 @@ var flowerspecies = {
     "sunflower": 5,
     "daisy": 21,
     "rose": 4
-}
+};
+
+var flowerspeciesarr = [{
+    "id": 1,
+    "name": "sunflower",
+    "quantity": 5
+}, 
+{
+    "id": 2,
+    "name": "daisy",
+    "quantity": 21
+},
+{
+    "id": 3,
+    "name": "rose",
+    "quantity": 4
+}];
 
 //Search for flowers in stock.
 app.get('/search/:flowers', (req, res) => {
     let flower = req.params.flowers;
     var reply;
-    if(flowerspecies[flower]){
+    if (flowerspecies[flower]) {
         reply = {
             status: "In stock",
             flower: flower,
             quantity: flowerspecies[flower]
         }
-    } 
+    }
     else {
         reply = {
             status: "Not in stock",
@@ -43,7 +59,7 @@ app.get('/add/:flowerspecie/:quantity', (req, res) => {
         reply = {
             msg: "Quantity is required."
         }
-    } 
+    }
     else {
         //Adding a flower (key value pair).
         flowerspecies[fs] = q;
@@ -54,13 +70,15 @@ app.get('/add/:flowerspecie/:quantity', (req, res) => {
     res.send(reply);
 });
 
-
-
 app.get('/flowerspecies', (req, res) => {
     res.send(flowerspecies);
 });
 
+app.get('/flowerspeciesarr', (req, res) => {
+    res.send(flowerspeciesarr);
+})
 
-app.use(express.static('public')); 
+
+app.use(express.static('public'));
 
 
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,24 @@
 $(document).ready(() => {
-    const url = "/flowerspecies"
-    $.getJSON(url, (data) => {
+    //JSON object.
+    const urlspecies = "/flowerspecies";
+    $.getJSON(urlspecies, (data) => {
+        //For key value pairs.
         $.each(data, (key, val) => {
-            console.log(key + val);
+            $('<li>' + key + '</li>').appendTo('#ul-flowers');
+            $('<li>' + val + '</li>').appendTo('#ul-stock');
         });
     });
+    //JSON array.
+    const urlspeciesarr = "/flowerspeciesarr";
+    $.getJSON(urlspeciesarr, (data) => {
+        for (let i = 0; i < data.length; i++) {
+            $('<li>' + data[i].id + '</li>').appendTo('#ul-idarr');
+            $('<li>' + data[i].name + '</li>').appendTo('#ul-flowersarr');
+            $('<li>' + data[i].quantity + '</li>').appendTo('#ul-stockarr');
+
+        }
+    });
 });
+
+
+
